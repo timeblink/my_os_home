a039135b0abcc203c6699746c23ff4c5edbc0dcd
txlu <timeblink@icloud.com>
2025-04-10 07:53:47 +0800

    update

---
+++ Downloads/financial_affairs/untitled.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+'''
+===============================================================================
+ Copyright (c) 2025- txlu
+
+===============================================================================
+
+ @file
+ @brief
+ @details
+ @version 1.0
+ @author  lutx0528
+ @date    2025-03-24
+
+ Edit History
+ ------------------------------------------------------------------------------
+ DATE            NAME               DESCRIPTION
+ 2025-03-24      lutx0528           Create it.
+'''
+
+import os
+
+remaining_sum_file   = "remaining_sum.csv"
+running_account_file = "running_account.csv"
+balance_sheet_file   = "balance_sheet.csv"
+
+if __name__ == "__main__":
+    print("hello")
