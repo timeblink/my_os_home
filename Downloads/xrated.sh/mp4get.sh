9c12471df1a6f46e47c187f7f96b9f7f6ea6c8ed
txlu <timeblink@icloud.com>
2025-05-12 07:18:38 +0800

    modify script

--- Downloads/xrated.sh/mp4get.sh
+++ Downloads/xrated.sh/mp4get.sh
@@ -1,56 +1,62 @@
 #!/bin/bash
 
+sshdir=${HOME}/Videos/xrated.ssh
+mkdir -p ${sshdir}
+
 mp4dir=${HOME}/Videos/xrated.mp4
 mkdir -p ${mp4dir}
 
 tsdir=${HOME}/Videos/xrated.ts
 mkdir -p ${tsdir}
 
-# LIMIT_RATE="1400"
-LIMIT_RATE="800"
+ucdir=${HOME}/Videos/xrated.uc
+mkdir -p ${ucdir}
 
-w_count=0
-i_sleep=45
+trashd=${HOME}//Videos/xrated.trash
+mkdir -p ${trashd}
 
-while [ $w_count -le 10 ]
-do
-    let w_count++
+LIMIT_RATE=
+h=`date +'%H'`
+[ $h == '00' ] && LIMIT_RATE="1400"
+[ $h == '01' ] && LIMIT_RATE="1400"
+[ $h == '02' ] && LIMIT_RATE="1400"
+[ $h == '03' ] && LIMIT_RATE="1400"
+[ $h == '04' ] && LIMIT_RATE="1200"
+[ $h == '05' ] && LIMIT_RATE="1200"
+[ $h == '06' ] && LIMIT_RATE="1000"
+[ $h == '07' ] && LIMIT_RATE="800"
+[ $h == '08' ] && LIMIT_RATE="600"
+[ $h == '09' ] && LIMIT_RATE="400"
+[ $h == '10' ] && LIMIT_RATE="200"
+[ $h == '11' ] && LIMIT_RATE="200"
+[ $h == '12' ] && LIMIT_RATE="200"
+[ $h == '13' ] && LIMIT_RATE="400"
+[ $h == '14' ] && LIMIT_RATE="200"
+[ $h == '15' ] && LIMIT_RATE="200"
+[ $h == '16' ] && LIMIT_RATE="200"
+[ $h == '17' ] && LIMIT_RATE="200"
+[ $h == '18' ] && LIMIT_RATE="200"
+[ $h == '19' ] && LIMIT_RATE="200"
+[ $h == '20' ] && LIMIT_RATE="200"
+[ $h == '21' ] && LIMIT_RATE="600"
+[ $h == '22' ] && LIMIT_RATE="800"
+[ $h == '23' ] && LIMIT_RATE="1400"
 
-    pushd ${mp4dir}
-    m3u8list=(`find -name '*.m3u8' -printf '%P\n'|sort -r`)
-    popd
+#pushd ${tsdir}
+rm -f *.ts playlist.m3u8
 
-    pushd ${tsdir}
-    for m3u8 in ${m3u8list[@]}
-    do
-        rm -f *.ts playlist.m3u8
-        name=${m3u8%%.*}
-        printf "[ -f %s.mp4 ]\t" "${name}"
-        if [ -f ${mp4dir}/${name}.mp4 ]
-        then
-            printf "\n"
-            continue
-        fi
-        printf "wget --quiet --limit-rate=%sk -c -i %s.txt\n" "${LIMIT_RATE}" "${name}"
-        wget --quiet --limit-rate=${LIMIT_RATE}k -c -i ${mp4dir}/${name}.txt
-        cp ${mp4dir}/${name}.m3u8 playlist.m3u8
-        ffmpeg -loglevel repeat+level+fatal -i playlist.m3u8 -c copy ${mp4dir}/${name}.mp4
-        res=$?
-        if [ $res -eq 0 ]
-        then
-            w_count=0
-        fi
-        rm ${mp4dir}/${name}.m3u8
-        rm ${mp4dir}/${name}.txt
+name=$1
 
-    done
-    rm -f *.ts playlist.m3u8
-    popd
+# printf "[ -f %s.mp4 ]\t" "${name}"
 
-    if [ $w_count -ne 0 ]
-    then
-        printf "sleep %ss\n" "${i_sleep}"
-        sleep ${i_sleep}s
-    fi
+printf "wget --quiet --limit-rate=%sk -c -i %s.txt\n" "${LIMIT_RATE}" "${name}"
+wget --quiet --limit-rate=${LIMIT_RATE}k -c -i ${mp4dir}/${name}.txt
+cp ${mp4dir}/${name}.m3u8 playlist.m3u8
+ffmpeg -loglevel repeat+level+fatal -i playlist.m3u8 -c copy ${ucdir}/${name}.mp4
+res=$?
 
-done
+rm -f *.ts playlist.m3u8
+#popd
+
+mv ${mp4dir}/${name}.m3u8 ${trashd}/
+mv ${mp4dir}/${name}.txt ${trashd}/
