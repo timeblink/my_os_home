9828e7b502129172a83019f8e76250d0cd75cf38
txlu <timeblink@icloud.com>
2025-05-29 22:12:02 +0800

    update

--- Downloads/xrated.sh/init_cut.sh
+++ Downloads/xrated.sh/init_cut.sh
@@ -3,8 +3,8 @@
 grep -v '\--,--,--' cut.txt > 1.txt
 
 find -type f -name '*.mp4' -printf '%P\n' \
-	| awk -F'.' '{print $1}' \
-	| awk -F'/' '{print $1","$2",--,--,--"}' > 2.txt 
+    | awk -F'.' '{print $1}' \
+    | awk -F'/' '{print $1","$2",--,--,--"}' > 2.txt 
 
 
 rm -f 11.txt
@@ -12,13 +12,13 @@ touch 11.txt
 
 while read line
 do
-	d0=$(echo $line | awk -F',' '{print $1}')
-	f=$(echo $line | awk -F',' '{print $2}')
-	h=$(echo $line | awk -F',' '{print $3}')
-	m=$(echo $line | awk -F',' '{print $4}')
-	s=$(echo $line | awk -F',' '{print $5}')
-	d1=$(grep $f 2.txt | awk -F',' '{print $1}')
-	printf "%s,%s,%s,%s,%s\n" $d1 $f $h $m $s | tee -a 11.txt
+    d0=$(echo $line | awk -F',' '{print $1}')
+    f=$(echo $line | awk -F',' '{print $2}')
+    h=$(echo $line | awk -F',' '{print $3}')
+    m=$(echo $line | awk -F',' '{print $4}')
+    s=$(echo $line | awk -F',' '{print $5}')
+    d1=$(grep $f 2.txt | awk -F',' '{print $1}')
+    printf "%s,%s,%s,%s,%s\n" $d1 $f $h $m $s | tee -a 11.txt
 done < 1.txt
 
 
@@ -27,13 +27,13 @@ touch 22.txt
 
 while read line
 do
-	d0=$(echo $line | awk -F',' '{print $1}')
-	f=$(echo $line | awk -F',' '{print $2}')
-	grep $f 1.txt
-	ret=$?
-	if [ $ret == 1 ] ; then
-		printf "%s,%s,--,--,--\n" $d0 $f | tee -a 22.txt
-	fi
+    d0=$(echo $line | awk -F',' '{print $1}')
+    f=$(echo $line | awk -F',' '{print $2}')
+    grep $f 1.txt
+    ret=$?
+    if [ $ret == 1 ] ; then
+        printf "%s,%s,--,--,--\n" $d0 $f | tee -a 22.txt
+    fi
 done < 2.txt
 
 
