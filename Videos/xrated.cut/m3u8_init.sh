9817491df85d4379d53106a2da3b15e1aeff15c6
txlu <timeblink@icloud.com>
2025-10-16 09:48:44 +0800

    update

--- Videos/xrated.cut/m3u8_init.sh
+++ Videos/xrated.cut/m3u8_init.sh
@@ -3,7 +3,6 @@
 rm playlist1.m3u8
 touch playlist1.m3u8
 echo "#EXTM3U" > playlist1.m3u8
-find -type f -name '*.mp4' -printf '%P\n' \
+find "$(pwd)" -type f -name '*.mp4' -printf '%p\n' \
 	| sort -t'c' -k1 -rk2 \
-	| awk '{print "/home/zzz/Videos/xrated.cut/"$1}' \
 	| tee -a playlist1.m3u8
