97827afb253cfb3d5d6d6823c7825cca55c40287
txlu <timeblink@icloud.com>
2025-10-16 09:48:44 +0800

    update

--- Videos/xrated.ssh/m3u8_init.sh
+++ Videos/xrated.ssh/m3u8_init.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-rm playlist1.m3u8
+#set -x
+
+[ -f playlist1.m3u8 ] && rm playlist1.m3u8
 touch playlist1.m3u8
 echo "#EXTM3U" > playlist1.m3u8
-find /home/zzz/Videos/xrated.ssh -type f -name '*.mp4' -printf '%p\n' \
+find "$(pwd)" -type f -name '*.mp4' -printf '%p\n' \
 	| sort -t'/' -rk7 \
 	| tee -a playlist1.m3u8
 
-rm playlist2.m3u8
+[ -f playlist2.m3u8 ] && rm playlist2.m3u8
 touch playlist2.m3u8
 echo "#EXTM3U" > playlist2.m3u8
-cat filelist.txt | tee -a playlist2.m3u8
+[ -f filelist.txt ] && cat filelist.txt | tee -a playlist2.m3u8
 
-rm playlist3.m3u8
+[ -f playlist3.m3u8 ] && rm playlist3.m3u8
 touch playlist3.m3u8
 echo "#EXTM3U" > playlist3.m3u8
 diff playlist1.m3u8 playlist2.m3u8 \
