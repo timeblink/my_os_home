5027f15f557fca1b5b75f096eb79b034e5c2fd45
txlu <timeblink@icloud.com>
2025-12-23 11:28:44 +0800

    update

--- Downloads/xrated.sh/download_mp4.sh
+++ Downloads/xrated.sh/download_mp4.sh
@@ -18,19 +18,20 @@ mkdir -p ${trashd}
 LIMIT_RATE=
 h=`date +'%H'`
 LIMIT_RATE="100"
-[ $h == '00' ] && LIMIT_RATE="1400"
-[ $h == '01' ] && LIMIT_RATE="1400"
-[ $h == '02' ] && LIMIT_RATE="1400"
-[ $h == '03' ] && LIMIT_RATE="1400"
-[ $h == '04' ] && LIMIT_RATE="1400"
-[ $h == '05' ] && LIMIT_RATE="1400"
+LIMIT_RATE="1024"
+#[ $h == '00' ] && LIMIT_RATE="1400"
+#[ $h == '01' ] && LIMIT_RATE="1400"
+#[ $h == '02' ] && LIMIT_RATE="1400"
+#[ $h == '03' ] && LIMIT_RATE="1400"
+#[ $h == '04' ] && LIMIT_RATE="1400"
+#[ $h == '05' ] && LIMIT_RATE="1400"
 [ $h == '06' ] && LIMIT_RATE="800"
 [ $h == '07' ] && LIMIT_RATE="400"
 [ $h == '08' ] && LIMIT_RATE="200"
 [ $h == '21' ] && LIMIT_RATE="200"
-[ $h == '22' ] && LIMIT_RATE="800"
-[ $h == '23' ] && LIMIT_RATE="1400"
-LIMIT_RATE="1024"
+[ $h == '22' ] && LIMIT_RATE="200"
+#[ $h == '23' ] && LIMIT_RATE="1400"
+#LIMIT_RATE="1024"
 
 #pushd ${tsdir}
 rm -f *.ts playlist.m3u8
