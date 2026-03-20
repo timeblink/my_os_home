68512b556c7223b62997a547acd7bf625030f9f2
txlu <timeblink@icloud.com>
2026-03-21 07:09:00 +0800

    九二曰。见龙在田。利见大人。
    何谓也。子曰。龙德而正中者也。庸言之信。庸行之谨。闲邪存其诚。
    善世而不伐。德博而化。易曰。见龙在田。利见大人。君德也。
    
    周易禅解卷
    
    北天目道人蕅益智旭

--- Videos/XRated/.xrated.ssh/m3u8_init.sh
+++ Videos/XRated/.xrated.ssh/m3u8_init.sh
@@ -6,13 +6,21 @@ echo "#EXTM3U" > playlist0.m3u8
 while read line
 do
   printf "%s/%s\n" ${PWD} $line | tee -a playlist0.m3u8
-done < filelist.txt
+done < filelist0.txt
 
-#    | sort -t'/' -rk7 -k6 \
 rm playlist1.m3u8
 touch playlist1.m3u8
 echo "#EXTM3U" > playlist1.m3u8
+while read line
+do
+  printf "%s/%s\n" ${PWD} $line | tee -a playlist1.m3u8
+done < filelist1.txt
+
+#    | sort -t'/' -rk7 -k6 \
+rm playlist2.m3u8
+touch playlist2.m3u8
+echo "#EXTM3U" > playlist2.m3u8
 find "$(pwd)" -type f -name '*.mp4' -printf '%p\n' \
-    | sort -t'/' -rk7 -k6 \
-	| tee -a playlist1.m3u8
+    | sort -t'/' -k7 -k6 \
+	| tee -a playlist2.m3u8
 
